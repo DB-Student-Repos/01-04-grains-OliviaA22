9702970f1491f92e3711f839ca6ae2d2f585336a
Olivia22 <olivia_adaugo@yahoo.com>
2022-03-18 21:31:01 +0100

    my solution

--- grains.py
+++ grains.py
@@ -1,6 +1,14 @@
 def square(number):
-    pass
-
-
+    if number < 1 or number > 64:
+        # when the square value is not in the acceptable range        
+        raise ValueError("square must be between 1 and 64")
+    y = 0.5
+    for x in range (number):
+        y = y * 2
+    return int(y)
+    
 def total():
-    pass
+    y = 0
+    for x in range(1, 65):
+        y = y + square(x)
+    return y
